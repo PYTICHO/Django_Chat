81011f5da4e6bc13f67b2d7ee519ee7d7c6f0387
PYTICHO <kasaev_2006@inbox.ru>
2023-07-18 18:17:51 +0300

    Update settings.py

--- onlineChat/onlineChat/settings.py
+++ onlineChat/onlineChat/settings.py
@@ -20,12 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rg4@t=w=av$gwfgowwl8_%kv3r+lrz7725t$e@u@z#sj7r#5r&'
-
+SECRET_KEY = ''
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.57', '127.0.0.1',"192.168.1.58"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
